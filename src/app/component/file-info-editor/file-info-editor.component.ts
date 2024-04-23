46fe68b6a6c0a0ed9d5bb0f4b308044d6888f421
Andrei Dodu <andrei.dodu@hotmail.it>
2024-04-23 06:56:23 +0200

    fix: trim input

--- src/app/component/file-info-editor/file-info-editor.component.ts
+++ src/app/component/file-info-editor/file-info-editor.component.ts
@@ -23,29 +23,28 @@ export class FileInfoEditorComponent implements OnInit {
   }
   generateForm(data: FileMetaInfoBook | undefined) {
     return this.formBuilder.group({
-      title: [data?.title, Validators.maxLength(512)],
-      publisher: [data?.publisher, [Validators.maxLength(100)]],
-      author: [data?.authors, Validators.maxLength(100)],
-      description: [data?.description, [Validators.maxLength(4000)]],
-      publishedDate: [data?.publishedDate, [Validators.maxLength(50)]],
+      title: [data?.title.trim(), Validators.maxLength(512)],
+      publisher: [data?.publisher.trim(), [Validators.maxLength(100)]],
+      description: [data?.description.trim(), [Validators.maxLength(4000)]],
+      publishedDate: [data?.publishedDate.trim(), [Validators.maxLength(50)]],
       isbn10: [
-        data?.isbn10,
+        data?.isbn10.trim(),
         [Validators.maxLength(10), Validators.minLength(10)],
       ],
       isbn13: [
-        data?.isbn13,
+        data?.isbn13.trim(),
         [Validators.maxLength(13), Validators.minLength(13)],
       ],
       authors: [data?.authors, Validators.maxLength(500)],
-      note: [data?.note, Validators.maxLength(2000)],
+      note: [data?.note.trim(), Validators.maxLength(2000)],
       numberOfPages: data?.numberOfPages,
-      language: [data?.language, Validators.maxLength(10)],
+      language: [data?.language?.trim(), Validators.maxLength(10)],
       categoryList: [
-        data?.categoryList.map((cat) => cat.name).join(','),
+        data?.categoryList.map((category) => category.name.trim()).join(','),
         Validators.maxLength(1000),
       ],
       tagList: [
-        data?.tagList.map((tag) => tag.name).join(','),
+        data?.tagList.map((tag) => tag.name.trim()).join(','),
         Validators.maxLength(1000),
       ],
     });
