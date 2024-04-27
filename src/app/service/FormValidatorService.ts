a04f4aae9ed920563f77850fd22bffacd3e0f5ba
Andrei Dodu <andrei.dodu@hotmail.it>
2024-04-27 14:02:59 +0200

    fix: ui

--- src/app/service/FormValidatorService.ts
+++ src/app/service/FormValidatorService.ts
@@ -1,6 +1,6 @@
 import { Validators } from '@angular/forms';
 
-const REGEX_USERNAME = /^[A-Za-z][A-Za-z0-9_]{5,36}$/;
+const REGEX_USERNAME = /^[A-Za-z][A-Za-z0-9_]{4,36}$/;
 const REGEX_PASSWORD =
   /^(?=.*[0-9])(?=.*[!@#$%^&*_:;,.])(?=.*[a-z])(?=.*[A-Z])[a-zA-Z0-9!@#$%^&*_:;,.]{5,50}$/;
 
